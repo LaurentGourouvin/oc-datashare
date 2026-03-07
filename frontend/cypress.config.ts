f5af529597cf52890751d93c45681bc0c1ed27d3
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-03-07 08:47:37 +0100

    chore(test): add cypress coverage

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
     setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require('@cypress/code-coverage/task')(on, config)
+      return config
       // implement node event listeners here
     },
   },
