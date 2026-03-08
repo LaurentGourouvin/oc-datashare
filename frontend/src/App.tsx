e586f2c19b44b9db40c1bbbcc0bd374cfa95d86b
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-03-08 09:54:50 +0100

    feat(dashboard): implement dashboard with file list, filters and delete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,13 @@ import Register from './pages/Register/Register';
 import Download from "./pages/Download/Download.tsx";
 import Home from "./pages/Home/Home.tsx";
 import Upload from "./pages/Upload/Upload.tsx";
+import Dashboard from "./pages/Dashboard/Dashboard.tsx";
 
 export default function App() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/download/:token" element={<Download />} />
