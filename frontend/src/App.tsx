09efb61872c10c964c2dbfd7204aa3b50a66592a
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2026-03-09 20:34:17 +0100

    fix(download): fix guard for download page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
             <Route path="/dashboard" element={<AuthGuard><Dashboard /></AuthGuard>} />
             <Route path="/login" element={<GuestGuard><Login /></GuestGuard>} />
             <Route path="/register" element={<GuestGuard><Register /></GuestGuard>} />
-            <Route path="/download/:token" element={<GuestGuard><Download /></GuestGuard>} />
+            <Route path="/download/:token" element={<Download />} />
             <Route path="/upload" element={<AuthGuard><Upload /></AuthGuard>} />
         </Routes>
     );
